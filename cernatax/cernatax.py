4ef627e940ff26b5340587aa0c81178e7cf5265f
chenlingxi@ <chanlingxi@gmail.com>
2025-05-03 21:31:07 +0800

    merge ipynb into one & add comments

--- cernatax/cernatax.py
+++ cernatax/cernatax.py
@@ -1,5 +1,11 @@
 import os
 import pandas as pd
+import numpy as np
+import scanpy as sc
+from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
+from adjustText import adjust_text
 
 
 class CERNATAX():
@@ -54,3 +60,103 @@ class CERNATAX():
         df.columns = ['miRNA', 'ceRNA', 'species', 'database', 'type', 'miRNA_log2FC', 'ceRNA_log2FC']
         df['inference'] = 'loose'
         return df
+    
+    def plot_cohort_ceRNA_exp(self, adata, groupby, ceRNA_list):
+        sc.pl.violin(adata, ceRNA_list, groupby=groupby)
+
+    def cohort_ceRNA_corr(self, adata, ceRNA_axis_list):
+
+        data = []
+        for gene1, gene2 in ceRNA_axis_list:
+        
+            res = stats.pearsonr(adata[:, gene1].X.T[0], adata[:, gene2].X.T[0])
+            data.append(['All', gene1, gene2, 'Pearson Correlation', res.statistic, res.pvalue])
+    
+            res = stats.spearmanr(adata[:, gene1].X.T[0], adata[:, gene2].X.T[0])
+            data.append(['All', gene1, gene2, 'Spearman Correlation', res.correlation, res.pvalue])
+    
+            res = stats.kendalltau(adata[:, gene1].X.T[0], adata[:, gene2].X.T[0])
+            data.append(['All', gene1, gene2, "Kendall's tau", res.correlation, res.pvalue])
+    
+        corr_df = pd.DataFrame(data)
+        corr_df.columns = ['patient', 'gene1', 'gene2', 'correlation type', 'correlation', 'p value']
+        return corr_df
+    
+    def screen_GWAS_SNP(self, sig_snp_data, gene_df):
+        gene_count = {}
+        snp_list = []
+
+        for key, snp_row in sig_snp_data.iterrows():
+            SNP_chr = snp_row['CHR']
+            SNP_pos = int(snp_row['BP'])
+            
+            snp_row['gene'] = []
+            for gene, row in gene_df.iterrows():
+                if SNP_chr != row['chr']:
+                    continue
+                if SNP_pos < row['start'] or SNP_pos > row['end']:
+                    continue
+                #print(i, SNP_chr, SNP_pos, gene, row)
+                if gene in gene_count:
+                    gene_count[gene] += 1
+                else:
+                    gene_count[gene] = 1
+                snp_row['gene'].append(gene)
+            if len(snp_row['gene']) > 0:
+                snp_list.append(snp_row)
+                    
+        GWAS_gene_snp_df = pd.DataFrame(snp_list)
+        sub_gene_df = gene_df[gene_df.gene.isin(gene_count.keys())]
+        sub_gene_df['SNP_count'] = sub_gene_df.gene.apply(lambda x: gene_count[x])
+        return GWAS_gene_snp_df, sub_gene_df, gene_count
+
+    def plot_gene_GWAS_SNP(self, GWAS_gene_snp_df, gene_count, ceRNA_list, out_fn):
+
+        fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+        for i, gene in enumerate(ceRNA_list):
+            ax = axes[i]
+            df = GWAS_gene_snp_df[GWAS_gene_snp_df['Gene']==gene]
+            sns.scatterplot(df, 
+                            x='OR', y='-log10(p)', palette='Set2', #size=1,
+                        hue='SNP Type', ax=ax, 
+                        hue_order=['A>C', 'A>G', 'C>A', 'C>G', 'C>T', 'G>A', 'T>C', 
+                                'Small Insertion', 'Small Deletion'])
+            # Calculate y = -log10(0.05)
+            threshold = -np.log10(0.05)
+
+            # Add Horizontal Line
+            ax.set_title(f'{gene}(n={gene_count[gene]})')
+            ax.axhline(y=threshold, color='gray', linestyle='--', linewidth=2, label=f"p = 0.05")
+            #ax.xticks(rotation=10)
+
+            # Create text labels
+            texts = []
+            data = df[df['-log10(p)'] > 2]
+            for i in range(len(data)):
+                texts.append(ax.text(data['OR'].iloc[i], data['-log10(p)'].iloc[i], data['SNP'].iloc[i], fontsize=10))
+
+            # print(texts)
+            # Adjust the positions of the texts to prevent overlap
+            adjust_text(texts, ax=ax)
+
+
+        # Get handles and labels from both plots
+        handles1, labels1 = axes[0].get_legend_handles_labels()
+        handles2, labels2 = axes[1].get_legend_handles_labels()
+        axes[0].legend().remove()
+        axes[1].legend().remove()
+
+
+        # Combine handles and labels, ensuring no duplicates
+        handles = handles1 + handles2
+        labels = labels1 + labels2
+        unique_handles_labels = dict(zip(labels, handles))
+
+        # Add common legend
+        fig.legend(unique_handles_labels.values(), unique_handles_labels.keys(), 
+                loc='center left', bbox_to_anchor=(0.9, 0.5), ncol=1, frameon=False)
+        # Adjust spacing
+        #plt.tight_layout()
+        fig.savefig(out_fn, format='pdf')
+        plt.show()        
